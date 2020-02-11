a707b5a226fe424c7467ab27c477903a68e0247e
loyd0729 <lbabay0729@hotmail.com>
2020-02-11 12:27:43 -0500

    Add files via upload

---
+++ Flyer.java
@@ -0,0 +1,13 @@
+
+public class Flyer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Flying fly = new Flying("sitting", 2, 4, "on", "took-off");
+		
+		System.out.println("The plane is " + fly.onAir());
+
+	}
+
+}
---
+++ Flying.java
@@ -0,0 +1,87 @@
+
+
+public class Flying {
+	private String pilot;
+	private int numOfPilot;
+	private int passengers;
+	private String engine;
+	private String state;
+	
+	public Flying() {
+		pilot = "sitting";
+		numOfPilot = 2;
+		passengers = 4;
+		engine = "on";
+		state = "took-off";
+		
+	}
+	
+	public Flying(String pilot, int numOfPilot, int passengers, String engine, String state) {
+		this.pilot = pilot;
+		this.numOfPilot = numOfPilot;
+		this.passengers = passengers;
+		this.engine = engine;
+		this.state = state;
+		
+	}
+	
+	public void setPilot(String pilot) {
+		this.pilot = pilot;
+		
+	}
+	
+	public String getPilot() {
+		return pilot;
+		
+	}
+	
+	public void setNumOfPilot(int numOfPilot) {
+		this.numOfPilot = numOfPilot;
+		
+	}
+	
+	public int getNumofPilot() {
+		return numOfPilot;
+		
+	}
+	
+	public void setPassengers(int passengers) {
+		this.passengers = passengers;
+		
+	}
+	
+	public int getPassengers() {
+		return passengers;
+		
+	}
+	
+	public void setEngine(String engine) {
+		this.engine = engine;
+		
+	}
+	
+	public String getEngine() {
+		return engine;
+		
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+		
+	}
+	
+	public String state() {
+		return state;
+		
+	}
+	
+	public String onAir() {
+		if(pilot.equals("sitting") && (numOfPilot == 2) && 
+				(passengers == 4) && engine.equals("on") && state.equals("took-off")) {
+			return "Currently on on-air";
+		} else {
+			return "on hold";
+		}
+	}
+
+}
